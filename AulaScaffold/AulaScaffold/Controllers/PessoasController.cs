61784ce46e95a20b8a893879a764d35fec8a0c96
Antony Vilanova <115712032+AntonyVilanovaSilva@users.noreply.github.com>
2024-08-08 10:28:53 -0300

    08/08

---
+++ AulaScaffold/AulaScaffold/Controllers/PessoasController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AulaScaffold.Models;
+
+namespace AulaScaffold.Controllers
+{
+    public class PessoasController : Controller
+    {
+        private AulaScaffoldBDEntities db = new AulaScaffoldBDEntities();
+
+        // GET: Pessoa
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Pessoa.ToListAsync());
+        }
+
+        // GET: Pessoa/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pessoa pessoa = await db.Pessoa.FindAsync(id);
+            if (pessoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pessoa);
+        }
+
+        // GET: Pessoa/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pessoa/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id,nome,idade")] Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pessoa.Add(pessoa);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(pessoa);
+        }
+
+        // GET: Pessoa/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pessoa pessoa = await db.Pessoa.FindAsync(id);
+            if (pessoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pessoa);
+        }
+
+        // POST: Pessoa/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id,nome,idade")] Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pessoa).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(pessoa);
+        }
+
+        // GET: Pessoa/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pessoa pessoa = await db.Pessoa.FindAsync(id);
+            if (pessoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pessoa);
+        }
+
+        // POST: Pessoa/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Pessoa pessoa = await db.Pessoa.FindAsync(id);
+            db.Pessoa.Remove(pessoa);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
